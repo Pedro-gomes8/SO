3042e9c441e4662b8373d81756a62f322ba01b27
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-09-10 20:07:42 -0300

    Setting up initial structure

--- main.c
+++ main.c
@@ -6,8 +6,22 @@ Ubuntu 20.04 LTS
 #include <stdio.h>
 #include <pthread.h>
 
+pthread_t *clients;
+pthread_t barber;
+
+void *handle_barber()
+{
+    pthread_exit(NULL);
+}
+
+void *client(pthread_t customer)
+{
+    pthread_exit(NULL);
+}
+
 int main(int argc, char *argv[])
 {
+    // Getting Parameters from argv or request input
     int numberOfClients, numberOfChairs;
     printf("Welcome to our barber shop!\n");
     if (argc != 3)
@@ -22,6 +36,20 @@ int main(int argc, char *argv[])
         numberOfClients = atoi(argv[1]);
         numberOfChairs = atoi(argv[2]);
     }
+    // Creating barber thread
+    pthread_create(&barber, NULL, handle_barber, NULL);
 
+    // Creating Clients as well as allocating memory for each client
+    clients = (pthread_t *)malloc(sizeof(pthread_t) * numberOfClients);
+    int i;
+    for (i = 0; i < numberOfChairs; i++)
+    {
+        pthread_create(clients[i], NULL, client, clients[i]);
+    }
+    // Waiting for barber to exit
+    pthread_join(barber, NULL);
+
+    // Freeing memory space
+    free(clients);
     return 0;
 }
