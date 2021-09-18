ab0dd28ae99a13a56285f21739e5e4d6fd367bc4
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-09-17 21:20:34 -0300

    Done

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ Problem chosen: FIFO Barber Shop
 pthread_t *clients;
 pthread_t barber;
 
-//
+// Defining how long the haircut should last. Currently 0.5s.
 enum {secsToSleep = 0, NsectoSleep = 500000000};
 struct timespec haircutTime = {secsToSleep, NsectoSleep};
 
@@ -21,12 +21,10 @@ int amountOfCustomers = 0;
 pthread_mutex_t queueMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t BarberMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t WaitforBarber = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t WaitForClient = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_cond_t customerIsHere = PTHREAD_COND_INITIALIZER;
 pthread_cond_t barberIsDone = PTHREAD_COND_INITIALIZER;
 pthread_cond_t barberIsFree = PTHREAD_COND_INITIALIZER;
-pthread_cond_t ClientLeft = PTHREAD_COND_INITIALIZER;
 
 int BarberHasSleptOnce = 0; // Stop condition. Barber can sleep once (i.e. when he is the first created thread to execute, thus he shall sleep until a customer arrives), but if he sleeps a second time, that means the queue is empty.
 int BarberIsSleeping = 0; 
@@ -59,6 +57,7 @@ int queue_write(queue_t *queue, void* handle) {
     return 0;
 }
 // End of external code.
+
 queue_t customerQueue;
 
 // Barber Thread
@@ -68,38 +67,43 @@ void *handle_barber()
         pthread_mutex_lock(&queueMutex); // Prevents other threads changing the value of 'amountOfCustomers' while the barber is checking it
         if (amountOfCustomers == 0){
             pthread_mutex_unlock(&queueMutex);
-            if (BarberHasSleptOnce){
+            if (BarberHasSleptOnce){ // If there is no customer and the barber has already slept, he shall close the shop.
                 printf("Looks like there's no one else left in line. Im gonna sleep again and close the shop!\n");
                 pthread_exit(NULL);
             }
             pthread_mutex_unlock(&queueMutex);
+
+            // Waiting for client
             pthread_mutex_lock(&BarberMutex);
             BarberIsSleeping = 1;
             // Barber goes to sleep if he is the first thread to be executed (no client yet).
             pthread_cond_wait(&customerIsHere,&BarberMutex);
-            // When he wakes up;
+            // When he wakes up:
             BarberIsSleeping = 0;
             pthread_mutex_unlock(&BarberMutex);
         }
         else {
-            // If he is awake and there is a customer, he cuts the customer's hair and say goodbye to him. 
+            // If he is awake and there is a customer, he cuts the customer's hair and say goodbye to them. 
             pthread_mutex_unlock(&queueMutex);
             printf("Cutting Hair...\n");
             nanosleep(&haircutTime,NULL);
+
+            // Queue manipulation, the barber grabs the lock to manipulate the queue and the amount of customers in line.
             pthread_mutex_lock(&queueMutex);
+
             // Gets the customer's "name".
             void * threadid = queue_read(&customerQueue);
             int id = (int) (long) threadid;
             printf("\tThere we go! Have a nice day Mr.(id:%d)\n",id);
-            // pthread_mutex_unlock(&queueMutex);
-            // Changing the amount of customers left to offer haircut services as he just cut someone's hair.
-            // pthread_mutex_lock(&queueMutex);
+
+            // Changing the amount of customers left as he just cut someone's hair.
             amountOfCustomers = amountOfCustomers - 1;
             pthread_mutex_unlock(&queueMutex);
+
             // After he's finished with the current customer and the queue has been rearranged, he signals another customer.
             pthread_cond_signal(&barberIsFree);
 
-            switch (BarberHasSleptOnce) // If barber wasnt the first created thread to be executed, that means there was no need for him to sleep for the first time. In that case, the first time he'd sleep would be whenever the queue would be empty. Essentially, it means: "If barber hasnt slept up to this point, lie about it and say he has slept once."
+            switch (BarberHasSleptOnce) // If barber wasnt the first created thread to be executed, that means there was no need for him to sleep for the first time. In that case, the first time he'd sleep would be whenever the queue would be empty. Essentially, this block means: "If barber hasnt slept up to this point, lie about it and say he has slept once."
             {
             case 0:
                 BarberHasSleptOnce = 1;
@@ -120,30 +124,33 @@ void *client(void *threadID)
     // Freeing memory of the index.
     free(threadID);
 
+    // Queue and 'amountOfCustomers' manipulation, lock is necessary.
     pthread_mutex_lock(&queueMutex);
-    while (amountOfCustomers >= numberOfChairs){
+    while (amountOfCustomers >= numberOfChairs){ // If there's no empty chair:
         printf("\tOh no, looks like the shop is full,(id:%d) is going home\n",id);
         pthread_mutex_unlock(&queueMutex);
         pthread_exit(NULL);
     }
     amountOfCustomers = amountOfCustomers + 1;
     queue_write(&customerQueue,(void *)(long) id);
+
     printf("Customer (id:%d) enters the shop\n",id);
     if(BarberIsSleeping){
         pthread_cond_signal(&customerIsHere);
     }
     pthread_mutex_unlock(&queueMutex);
+    
+    // Haircut time!
     pthread_mutex_lock(&WaitforBarber);
     pthread_cond_wait(&barberIsFree,&WaitforBarber);
     // Barber begins cutting hair...
-    // when this thread wakes up:
     pthread_mutex_unlock(&WaitforBarber);
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-    printf("\nIMPORTANTE. Apos alguns testes, eu percebi que esse codigo possui uma falha que pode aparecer, ou nao, quando for testar que nao consegui identificar. De alguma forma, apenas uma unica thread (independente dos parametros que esse codigo recebe) 'perde' seu ID. seu ID passa a ser 0 como se fosse a primeira thread criada, e essa thread que perde o ID acaba no final da queue. Isso pode aparecer nos resultados. Embora uma unica thread possa perder, ou nao, seu lugar na fila, todas as threads que entram na barbearia continuam tendo seu cabelo cortado yay!\n\n");
+    printf("\nIMPORTANTE. Apos alguns testes, eu percebi que esse codigo possui uma falha que pode aparecer, quando for testar que nao consegui solucionar. De alguma forma, apenas uma unica thread (independente dos parametros que esse codigo recebe) 'perde' seu ID. Seu ID passa a ser 0 como se fosse a primeira thread criada, e essa thread que perde o ID acaba no final da queue. Isso pode aparecer nos resultados. Embora uma unica thread possa perder, ou nao, seu lugar na fila, todas as threads que entram na barbearia continuam tendo seu cabelo cortado yay!\n\n");
 
     // Getting Parameters from argv or request input
     printf("%s\n","Welcome to our barber shop!");
